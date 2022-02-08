29f94b74b36b98dac6550d27152a18ea628a777b
binhtran432k <binhtran432k@gmail.com>
2022-02-08 19:42:41 +0700

    Fix bufferline mapping

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,7 +10,7 @@ vim.cmd [[
 " These commands will navigate through buffers in order regardless of which mode you are using
 " e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
 nnoremap <silent>[b :BufferLineCycleNext<CR>
-nnoremap <silent>b] :BufferLineCyclePrev<CR>
+nnoremap <silent>]b :BufferLineCyclePrev<CR>
 
 " These commands will sort buffers by directory, language, or a custom criteria
 nnoremap <silent><leader>be :BufferLineSortByExtension<CR>
